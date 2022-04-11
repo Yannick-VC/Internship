bce15afac1b1efce6f2198eb6294c9956e582fab
Yannick Van Campenhout <36662635+YannickVC2@users.noreply.github.com>
2022-04-11 16:03:40 +0200

    var changes + security group moved

--- s1/server.tf
+++ s1/server.tf
@@ -1,45 +1,20 @@
 resource "aws_instance" "phpmyadmin" {
   ami             = "ami-0c6ebbd55ab05f070"
   instance_type   = "t2.micro"
-  #user_data	= file("./util/file.sh")
   user_data = data.template_file.init.rendered
-  security_groups = [aws_security_group.default_SG.name] 
- 
+  vpc_security_group_ids = [aws_security_group.phpinstance.id] 
+  subnet_id = aws_subnet.public_Subnet_1.id
   associate_public_ip_address = true
-
   tags = {
     Name = "PHPMyAdminInstance"
-  }
-}
-
-resource "aws_security_group" "default_SG" {
-  name        = "allow_all"
-  description = "Allow all inbound http(s) traffic"
-
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_all"
   }
 }
 
 data "template_file" "init" {
   template = "${file("./util/file.sh.tpl")}"
   vars = {
-    db_username = aws_db_instance.production.username
-    db_password = aws_db_instance.production.password
-    db_address = aws_db_instance.production.address
+    db_username = aws_db_instance.database.username
+    db_password = aws_db_instance.database.password
+    db_address = aws_db_instance.database.address
   }
 }
