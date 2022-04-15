d89535223b5b57ac11f732a61df9468399798adc
Yannick Van Campenhout <36662635+YannickVC2@users.noreply.github.com>
2022-04-15 16:03:39 +0200

    removals

--- s1/output.tf
+++ s1/output.tf
@@ -1,23 +1,3 @@
-output "DB-Username" {
-  description = "Username used for login of database"
-  value = aws_db_instance.production.username
-}
-
-output "DB-Password" {
-  description = "Password used for login of database"
-  value = "password123"
-}
-
-output "DB-Address" {
-  description = "Address used for login of database"
-  value = aws_db_instance.production.address
-}
-
-output "DB-Port" {
-  description = "Port used for login of database"
-  value = aws_db_instance.production.port
-}
-
 output "Public_IP" {
   description = "The public IP address assigned to the instance"
   value = aws_instance.phpmyadmin.public_ip
