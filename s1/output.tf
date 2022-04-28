25ddf990e009f9555f138220a251374e8976d621
Yannick Van Campenhout <36662635+YannickVC2@users.noreply.github.com>
2022-04-28 12:29:31 +0200

    acc id

--- s1/output.tf
+++ s1/output.tf
@@ -2,3 +2,8 @@ output "Public_IP" {
   description = "The public IP address assigned to the instance"
   value = aws_instance.phpmyadmin.public_ip
 }
+
+output "Account ID" {
+  description = "The account ID needed to log into the AWS account"
+  value = aws_vpc.main_VPC.owner_id
+}
